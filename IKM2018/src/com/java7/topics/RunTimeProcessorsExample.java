e28e248f6b2605d3d2d02b2737340ac475a040ff
gitswetham <veerankipavan123@gmail.com>
2018-05-23 22:55:32 +1000

    Removed folder IKM2018 from repository

--- IKM2018/src/com/java7/topics/RunTimeProcessorsExample.java
+++
@@ -1,9 +0,0 @@
-package com.java7.topics;
-
-public class RunTimeProcessorsExample {
-
-    public static void main(String args[]) {
-
-        System.out.println("Available Processors in Java : "+Runtime.getRuntime().availableProcessors());
-    }
-}
--- IKM2018/src/com/java7/topics/StatusEnum.java
+++
@@ -1,5 +0,0 @@
-package com.java7.topics;
-
-public enum StatusEnum {
-     NEW, EXECUTE, PENDING
-}
